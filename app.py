ac0dcefa7746af537dfb07c42d9741b0c520edc3
Omarxp69 <135158823+Omarxp69@users.noreply.github.com>
2025-10-17 22:11:44 -0600

    cambio2

--- app.py
+++ app.py
@@ -1307,5 +1307,15 @@ def procesar_foto(file, folder, prefijo, id_item):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
+    from flask import Flask
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return "Hello World"
+
+
+
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host="0.0.0.0", port=port)
